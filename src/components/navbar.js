497e2b8ddccf472a4e9b7241b180196cde786605
jelenarankovic <jrankovic13@gmail.com>
2021-03-28 15:41:31 +0200

    navbar i dugme

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
      const [click, setClick] = useState(false);
-    const handleClick= () => setClick(!click);
+    const[button, setButton] = useState(true);
+     const handleClick= () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-     return (
+    const showButton = () => {
+        if(window.innerWidth<= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    }; 
+
+    useEffect(() =>{
+        showButton()
+    }, []);
+    window.addEventListener('resize', showButton);
+    return (
        <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     TRVL <i className='fab fa-typo3'/>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
@@ -40,6 +54,8 @@ function Navbar() {
                         </li>
 
                     </ul>
+
+                    {button && <Button buttonStyle='btn--outline'>Uloguj se</Button>}
 
             </div>
         </nav>
